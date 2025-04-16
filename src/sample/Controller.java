5815d4b803b9683e908533e1f7d755c0500aa0d2
wellsnap <danieltranca5@gmail.com>
2025-04-15 20:58:39 -0400

    second commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,79 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import java.util.Date;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Label timeLabel;
+    @FXML
+    private Label displayUsername;
+
+    @FXML
+    private Text total;
+    @FXML
+    private Text wpm;
+    @FXML
+    private Text invalid;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        File newFile = new File("username.txt");
+        if (newFile.length() != 0) {
+            Scanner reader = null;
+            try {
+                reader = new Scanner(newFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            String data = reader.nextLine();
+            displayUsername.setText("Welcome,"+data);
+
+        }
+        // set the day
+        Date date = new Date();
+        Locale locale = new Locale("en");
+        DateFormat formatter = new SimpleDateFormat("EEEE", locale);
+        String strDay = formatter.format(date);
+
+        timeLabel.setText("Today is " + strDay);
+
+
+        // we need to display data
+    }
+
+
+
+
+
+    public void playGame(ActionEvent ae) throws IOException {
+        Main m = new Main();
+
+        File newFile = new File("username.txt");
+        if (newFile.length() == 0) {
+            try {
+                m.changeScene("popup.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            m.changeScene("game.fxml");
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Stage stg;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stg = primaryStage;
+        primaryStage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Type Test");
+        primaryStage.getIcons().add(new Image("images/icon.png"));
+        primaryStage.setScene(new Scene(root, 600, 450));
         primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 
 
